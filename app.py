4a8c879f2556f18ec8c1228be13d77b8e70c5e6d
Fatima Abuelelaa Ibrahim <Fatima.Ibrahim@moe.gov.ae>
2020-05-07 10:19:37 +0400

    make it better

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/<msg>')
-def indix(msg): 
-    sentiment = "positive"
-    if TextBlob(msg).sentiment.polarity < 0.0:
-        sentiment ="negative"
-    return app.make_response(f"<h1> {sentiment} </h1>")
+# @app.route('/<msg>')
+def indix(): 
+    # sentiment = "positive"
+    # if TextBlob(msg).sentiment.polarity < 0.0:
+    #     sentiment ="negative"
+    # # return app.make_response(f"<h1> {sentiment} </h1>")
+    return "deployed to Heroku"
 
 
 
